1c363769c4557616d798fb2576d1852b79fdc267
owen-6936 <erhaborowen3@gmail.com>
2025-10-06 18:44:03 +0100

    Add shell session management and command execution functionality

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module llm-cortex
 
 go 1.22.2
+
+require github.com/google/uuid v1.6.0 // indirect
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"llm-cortex/router"
+	"llm-cortex/spawn"
 	"net/http"
 )
 
@@ -10,10 +11,40 @@ func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", rootHandler)
 
-	fmt.Println("Starting server at port 8080")
-	err := http.ListenAndServe(":8080", mux)
+	// fmt.Println("Starting server at port 8080")
+	// err := http.ListenAndServe(":8080", mux)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	var sessions = make(map[string]*spawn.ShellSession)
+	sessionId, err := spawn.NewShell(sessions)
 	if err != nil {
 		panic(err)
+	}
+
+	spawn.StartReading(sessions[sessionId], spawn.OutputHandler)
+
+	fmt.Println("\n--- Sending Command 2: 'pwd' ---")
+	sendCmdErr := spawn.SendCommand(sessions, sessionId, "pwd")
+	if sendCmdErr != nil {
+		fmt.Println("Error sending command:", err)
+	}
+
+	fmt.Println("\n--- Sending Command 2:  ---")
+	sendCmdErr2 := spawn.SendCommand(sessions, sessionId, "echo hello from Owen")
+	if sendCmdErr2 != nil {
+		fmt.Println("Error sending command:", err)
+	}
+
+	fmt.Println("\n--- Sending Command 3: ---")
+	sendCmdErr3 := spawn.SendCommand(sessions, sessionId, "echo Hmm imagine if this is from an LLM")
+	if sendCmdErr3 != nil {
+		fmt.Println("Error sending command:", err)
+	}
+
+	fmt.Println("\n--- Closing Session ---")
+	if err := spawn.CloseSession(sessions, sessionId); err != nil {
+		fmt.Println("Error closing session:", err)
 	}
 }
 
